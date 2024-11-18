b6557b24847fbaf39d07e3fb013f81a5a0c10e03
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-11-18 13:42:44 -0500

    generate spring initializer project files

---
+++ src/main/java/com/example/brandon/photos_app/PhotosAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.brandon.photos_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotosAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/brandon/photos_app/PhotosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brandon.photos_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
