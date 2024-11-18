64cbfb2b15729719cfa9696cd56d58e85e0505ce
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-11-18 16:48:54 -0500

    change list to collection and setup id route

--- src/main/java/com/example/brandon/photos_manager/PhotosManagerController.java
+++ src/main/java/com/example/brandon/photos_manager/PhotosManagerController.java
@@ -1,15 +1,23 @@
 package com.example.brandon.photos_manager;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 
 public class PhotosManagerController {
 
-    private List<PhotoModel> db = List.of(new PhotoModel("1", "hello.jpg"));
+    private Map<String, PhotoModel> db = new HashMap<>() {{
+        put("1", new PhotoModel("1", "hello.jpg"));
+    }};
 
     @GetMapping("/")
     public String hello() {
@@ -17,8 +25,17 @@ public class PhotosManagerController {
     }
 
     @GetMapping("/photos")
-    public List<PhotoModel> get() {
-        return db;
+    public Collection<PhotoModel> get() {
+        return db.values();
+    }
+
+    @GetMapping("/photos/{id}")
+    public PhotoModel get(@PathVariable String id) {
+        PhotoModel photo = db.get(id);
+
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return photo;
     }
 
 }
